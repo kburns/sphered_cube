d0569ce9c4a4c58a4aa3169163cd3ee562fe3b22
Keaton J. Burns <keaton.burns@gmail.com>
2019-03-24 16:19:16 -0400

    Slice before converting to xarray

--- scripts/plot_vert_slice.py
+++ scripts/plot_vert_slice.py
@@ -2,10 +2,9 @@
 Plot planes from joint analysis files.
 
 Usage:
-    plot_vert_slices.py <files>... [--noextrap] [--output=<dir>]
+    plot_vert_slices.py <files>... [--output=<dir>]
 
 Options:
-    --noextrap      Don't extrapolate to coordinate endpoints
     --output=<dir>  Output directory [default: ./frames]
 
 """
@@ -23,46 +22,77 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def hdf5_to_xarray(h5_data):
-    """Convert hdf5 dataset to xarray."""
-    data = h5_data[:]
-    coords = [(dim.label, dim[0][:]) for dim in h5_data.dims]
-    xr_data = xr.DataArray(data, coords=[coords[0], coords[3], coords[2], coords[1]])
-    return xr_data
-
-def load_task_xarray(filename, task, skip=1):
-    """Load saved task as xarray."""
+def load_task_hdf5(filename, task):
+    """Load task from hdf5."""
     # Load file
     file = h5py.File(filename, 'r')
-    # Get data
-    xr = hdf5_to_xarray(file['tasks'][task])
-    return xr
+    return file['tasks'][task]
 
-def extrap_full_sphere(X):
-    """Extrapolate spherical grid data to singularities."""
-    # Extrapolate in theta
+
+def hdf5_to_xarray(h5_data, slices=None):
+    """Convert hdf5 dataset to xarray."""
+    if slices is None:
+        slices = (slice(None),) * len(h5_data.shape)
+    data = h5_data[slices]
+    coords = [(dim.label, dim[0][:]) for dim, s in zip(h5_data.dims, slices)]
+    coords = [coords[0], coords[3], coords[2], coords[1]]
+    coords = [(c[0], c[1][s]) for c,s in zip(coords, slices)]
+    xr_data = xr.DataArray(data, coords=coords)
+    return xr_data
+
+
+def extrap_theta(X):
     X_0 = X.interp(theta=0, kwargs={'fill_value': 'extrapolate'})
     X_pi = X.interp(theta=np.pi, kwargs={'fill_value': 'extrapolate'})
-    X = xr.concat([X_pi, X, X_0], dim='theta')
+    return xr.concat([X_0, X, X_pi], dim='theta')
+
+
+def extrap_r(X):
     # Extrapolate in r
     X_0 = X.interp(r=0, kwargs={'fill_value': 'extrapolate'})
-    X = xr.concat([X_0, X], dim='r')
+    return xr.concat([X_0, X], dim='r')
+
+
+def extrap_phi(X):
+    X_2pi = X.interp(phi=2*np.pi, kwargs={'fill_value': 'extrapolate'})
+    return xr.concat([X, X_2pi], dim='phi')
+
+
+def hdf5_vert_slice_to_xarray(X_hdf5, phi):
+    # Get phi indeces
+    N_phi = X_hdf5.shape[1]
+    phi0 = int(np.round(phi / 2 / np.pi * N_phi))
+    phi1 = int(np.round((phi + np.pi) / 2 / np.pi * N_phi))
+    # Get phi slices
+    slices = (slice(None), slice(phi0, phi0+1), slice(None), slice(None))
+    X0 = hdf5_to_xarray(X_hdf5, slices)
+    slices = (slice(None), slice(phi1, phi1+1), slice(None), slice(None))
+    X1 = hdf5_to_xarray(X_hdf5, slices)
+    # Concatenate and reorder
+    X = xr.concat((X0, X1), dim='phi')
+    X = X.sortby('phi')
     return X
 
 
-def main(filename, output_path, extrapolate=True):
+def main(filename, output_path):
 
     logger.info('Plotting from file: %s' %filename)
 
-    # Plot settings
+    # Plot settings    
+    phi = 0
     dpi = 200
-    cmap = 'RdBu'
+    cmap = 'RdBu_r'
     plt.figure(figsize=(6,6))
-
-    # Load temperature perturbation
-    T = load_task_xarray(filename, 'T')
-    if extrapolate:
-        T = extrap_full_sphere(T)
+    
+    # Load temperature perturbation from hdf5
+    filename = '/Users/kburns/Dropbox/projects/sphered_cube/rbc_s1.h5'
+    T_hdf5 = load_task_hdf5(filename, 'T')
+    
+    # Load xarray slice
+    T = hdf5_vert_slice_to_xarray(T_hdf5, phi)
+    T = T.reindex(theta=T.theta[::-1])
+    T = extrap_r(T)
+    T = extrap_theta(T)
 
     # Add cartesian coords
     T.coords['x'] = T.coords['r'] * np.sin(T.coords['theta']) * np.cos(T.coords['phi'])
@@ -73,23 +103,22 @@ def main(filename, output_path, extrapolate=True):
     T0 = -T.coords['z']
     # Box half-side length
     s = 1 / np.sqrt(3)
-    # Azimuthal selections
-    phi0 = 0
-    phi1 = int(T.coords['phi'].size / 2)
 
+    # Plot writes
     for i in range(T.shape[0]):
         logger.info('  Plotting from write: %i' %i)
         plt.clf()
         # Plot total temperature
         Ti = T0 + T.isel(t=i)
         vmax = np.max(np.abs(Ti))
-        Ti.isel(phi=phi0).plot(x='x', y='z', add_colorbar=False, vmin=-vmax, vmax=vmax, cmap=cmap)
-        Ti.isel(phi=phi1).plot(x='x', y='z', add_colorbar=False, vmin=-vmax, vmax=vmax, cmap=cmap)
+        Ti.isel(phi=0).plot(x='x', y='z', add_colorbar=False, vmin=-vmax, vmax=vmax, cmap=cmap)
+        Ti.isel(phi=1).plot(x='x', y='z', add_colorbar=False, vmin=-vmax, vmax=vmax, cmap=cmap)
         plt.title('')
         plt.axis('equal')
         plt.axis('off')
         plt.savefig(str(output_path.joinpath('sphere_%03i.png' %i)), dpi=dpi)
         # Save with box
+        ## WRONG FOR PHI != 0
         line, = plt.plot([s,s,-s,-s,s], [-s,s,s,-s,-s], '--k', lw=1)
         plt.savefig(str(output_path.joinpath('spherebox_%03i.png' %i)), dpi=dpi)
         line.set_visible(False)
@@ -111,7 +140,6 @@ if __name__ == "__main__":
     args = docopt(__doc__)
     files = args['<files>']
     output_path = pathlib.Path(args['--output']).absolute()
-    extrapolate = not args['--noextrap']
 
     # Create output directory if needed
     with Sync() as sync:
@@ -124,5 +152,5 @@ if __name__ == "__main__":
     size = MPI.COMM_WORLD.size
     local_files = files[rank::size]
     for file in local_files:
-        main(file, output_path, extrapolate)
+        main(file, output_path)
 
