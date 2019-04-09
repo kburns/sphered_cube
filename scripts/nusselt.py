Fix nusselt normalization

scripts/nusselt.py
@@ -14,7 +14,7 @@ end = 1000
 dir = params.snapshots_dir
 
 SB = SimpleBall(params.R, params.L_max, params.N_max, params.R_max, params.L_dealias, params.N_dealias, comm=MPI.COMM_SELF)
-weight_r = SB.weight_r / SB.r
+weight_r = SB.weight_r
 
 N_theta = params.L_dealias * (params.L_max + 1)
 N_phi = 2 * N_theta
@@ -39,6 +39,8 @@ for i in range(start+rank, end, size):
         Nu = np.sum(w_mid * T_mid, axis=0) * 2 * np.pi / N_phi
         # Integrate over r
         Nu = np.sum(weight_r * Nu)
+        # Normalize by box area
+        Nu = Nu / params.L**3
         Nu_list.append(Nu)
         t_list.append(f['scales/sim_time'][j])
 

scripts/parameters.py
@@ -2,10 +2,10 @@
 import numpy as np
 
 # Parallelization
-mesh = None#[12,32]
+mesh = [12,32]
 
 # Spatial discretization
-N = 31
+N = 127
 L_max = N
 N_max = N
 R_max = 3
@@ -18,13 +18,14 @@ R = np.sqrt(3) / 2
 L = 1
 dx = R / N
 Prandtl = 1
-Danger = 20
-Rayleigh = Danger * Prandtl * (L / dx)**(8/3)
+Ra_danger = 20
+Rayleigh = Ra_danger * Prandtl * (L / dx)**(8/3)
 t_ff = (Rayleigh * Prandtl)**(-1/2)
 
 # Volume penalization
-epsilon = (L / dx)**2
-delta = dx
+ep_danger = 100
+epsilon = ep_danger * (L / dx)**2
+delta = dx / ep_danger**0.5
 
 # Initial conditions
 amp = 1e-3
@@ -36,7 +37,7 @@ safety = 0.4
 threshold = 0.1
 
 # Outputs
-snapshots_dir = 'snapshots'
+snapshots_dir = 'rbc'
 snapshots_cadence = t_ff/10
 
 